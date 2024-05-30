2c8d2487ba83e7211d6d816a3d1fa63a327e3cff
gracecavanagh <151089198+gracecavanagh@users.noreply.github.com>
2024-05-29 21:40:58 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
 const parallax_el = document.querySelectorAll(".parallax");
 
 let xValue = 0, 
-  yValue = 0;
+  yValue = 0; 
 
 window. addEventListener("mousemove", (e) => {
   xValue = e.clientX - window.innerWidth / 2;
   vValue = e.clientY - window.innerHeight / 2;
 
+  let zValue = e.clientX - getComputedStyle(el).left;
+
   parallax_el.forEach(el => {
     let speedx = el.dataset.speedx;
     let speedy = el.dataset.speedy;
-    el.style.transform = 'translateX(calc(-50% + ${-xValue * speedx}px)) translateY (calc(-50% + ${yValue * speedy}px))';
+    el.style.transform = 'translateX(calc(-50% + ${
+      -xValue * speedx
+      }px)) translateY(calc(-50% + ${
+      yValue * speedy
+      }px)) perspective(2300px) translateZ(${yValue}px)';
 });
