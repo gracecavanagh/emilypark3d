1ff7be7dbe0b98e895891278734549a6e880e56d
gracecavanagh <151089198+gracecavanagh@users.noreply.github.com>
2024-05-29 21:02:47 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,5 +8,6 @@ window. addEventListener("mousemove", (e) => {
   vValue = e.clientY - window.innerHeight / 2;
 
   parallax_el.forEach(el => {
-    el.style.transform = 'translateX(calc(-50% + ${-xValue}px)) translateY (calc(-50% + ${yValue}px))';
+    let speedx = el.dataset.speedx;
+    el.style.transform = 'translateX(calc(-50% + ${-xValue * speedx}px)) translateY (calc(-50% + ${yValue}px))';
 });
